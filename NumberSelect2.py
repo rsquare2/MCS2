8b05aa517616736468c304c20111e873cb804155
aThunder <rr234rr@gmail.com>
2015-11-22 16:23:20 -0500

    simplified process byusing iteration to check all numbers with at least one match

--- NumberSelect2.py
+++ NumberSelect2.py
@@ -1,11 +1,9 @@
 
 # NumberSelect.py
 
-"""for each number 1-10 pick random number ("pick") and see if there is a match.
-     If there is a match increase "match" by one.
-     Divide total matches over specified number of rounds and divide by total rounds.
-     Creates List to summarize number of matches for each of 1-10
-
+"""Iterates through 1-10 & picks a random number each iteration and see if there is a match.
+   Creates a List to summarize the number of matches for each of 1-10
+   and uses that List to build a dictionary
 """
 
 import random
@@ -17,12 +15,13 @@ class Compare:
         self.match = 0
         self.maxMatchesSoFar = 0
         self.overallMatchesList = []
-        self.match0 = 0
-        self.match1 = 0
-        self.match2 = 0
-        self.match3 = 0
+        # self.match0 = 0
+        # self.match1 = 0
+        # self.match2 = 0
+        # self.match3 = 0
         self.fullResults = {}
-        self.fullResults2 = {}
+        self.keyList = []
+        self.valueList = []
 
     def selectRandom(self):
         for round in range(self.rounds):
@@ -35,24 +34,17 @@ class Compare:
 
                 if self.maxMatches > self.maxMatchesSoFar:
                     self.maxMatchesSoFar = self.maxMatches
-
-            ## next 2 lines are beginning of work to skip list and create
-            ## Dictionary directly       
-            # print(self.maxMatches)
-            # self.fullResults2[self.maxMatches] = 1
-
-
-            if self.maxMatches == 0:
-               self.match0 += 1
-            if self.maxMatches == 1:
-                   self.match1 += 1
-            if self.maxMatches == 2:
-                   self.match2 += 1
-            if self.maxMatches == 3:
-               self.match3 += 1
+            #
+            # if self.maxMatches == 0:
+            #    self.match0 += 1
+            # if self.maxMatches == 1:
+            #        self.match1 += 1
+            # if self.maxMatches == 2:
+            #        self.match2 += 1
+            # if self.maxMatches == 3:
+            #    self.match3 += 1
 
             self.overallMatchesList.append(self.maxMatches)
-
 
         for i in self.overallMatchesList:
             if i not in self.fullResults:
@@ -61,19 +53,33 @@ class Compare:
             else:
                 self.fullResults[i] = self.fullResults[i] + 1
 
+        for key in self.fullResults:
+            self.keyList.append(key)
+            # print('key: ',self.keyList)
+
+        for value in range(len(self.fullResults)):
+            self.valueList.append(self.fullResults[value])
+            # print('value: ',self.valueList)
+
+
+
+
         print("Dictionary of Results: ",self.fullResults)
-        print ("DictionaryBeta of Results: ",self.fullResults2)
         print("Total Cumulative Matches : ",self.match)
         print("Rounds: ", self.rounds)
         print("Avg Matches Per Round: ", (self.match/self.rounds))
         print("Max Matches in a Round: ", self.maxMatchesSoFar)
-        print("0 Match Rounds: {0}%".format(int((self.match0/self.rounds)*100)))
-        print("1 Match Rounds: {0}%".format(int((self.match1/self.rounds)*100)))
-        print("2 Match Rounds: {0}%".format(int((self.match2/self.rounds)*100)))
-        print("3 Match Rounds: {0}%".format(int((self.match3/self.rounds)*100)))
+
+        for counter in range(len(self.keyList)):
+            print("{0} Match Rounds: {1}%".format(self.keyList[counter],(self.valueList[counter]/self.rounds)*100))
+
+        # print("0 Match Rounds: {0}%".format(int((self.match0/self.rounds)*100)))
+        # print("1 Match Rounds: {0}%".format(int((self.match1/self.rounds)*100)))
+        # print("2 Match Rounds: {0}%".format(int((self.match2/self.rounds)*100)))
+        # print("3 Match Rounds: {0}%".format(int((self.match3/self.rounds)*100)))
 
 def main():
-    a = Compare(5)
+    a = Compare(10000)
     b = a.selectRandom()
 
 if __name__ == '__main__': main()
